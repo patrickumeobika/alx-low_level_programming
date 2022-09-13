9535632e5bcfc59272bcfab82bf9a346bfcf0747
patrickumeobika <umeobikachidieberembj16@gmail.com>
2022-09-13 21:32:56 +0000

    task 8,10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adding it up
+ *
+ * @i: first integer
+ *
+ * @j: second integer
+ *
+ * Return: sum of both integers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
