ddec62061183f83988529dc62bb8882b3f48f22a
patrickumeobika <umeobikachidieberembj16@gmail.com>
2022-09-13 21:14:13 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all alphabets
+ *
+ *@void: Reurns nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
