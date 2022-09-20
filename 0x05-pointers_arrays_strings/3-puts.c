ec8b2bddc1b45514db0b59af08fb4015d9f08f0c
patrickumeobika <umeobikachidieberembj16@gmail.com>
2022-09-20 00:36:10 +0000

    task 8

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,23 +1,9 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * _puts - prints a string, followed by a newline to stdout
  * @str: string parameter to print
  */
-
-void _puts(char *str)
-{
-	while (*str != '\0')
-		_putchar(*str++);
-
-	_putchar('\n');
-}
-#include "main.h"
-
- /**
-  * _puts - prints a string, followed by a newline to stdout
-  * @str: string parameter to print
-  */
 
 void _puts(char *str)
 {
