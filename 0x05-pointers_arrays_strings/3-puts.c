162caaa14bb0a747b7e097a81c98f69d9aa7d73f
patrickumeobika <umeobikachidieberembj16@gmail.com>
2022-09-20 00:29:58 +0000

    task 5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * _puts - prints a string, followed by a newline to stdout
@@ -11,7 +11,8 @@ void _puts(char *str)
 		_putchar(*str++);
 
 	_putchar('\n');
-}#include"main.h"
+}
+#include "main.h"
 
  /**
   * _puts - prints a string, followed by a newline to stdout
