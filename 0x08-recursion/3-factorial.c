7f1e777dc42dcdbe3c2c5bd1dd0eee1a3c541aee
patrickumeobika <umeobikachidieberembj16@gmail.com>
2022-09-29 04:31:30 +0000

    task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - gives factorial of a given number
+ * @n: number to accept
+ * Return: returns the factor or -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	return (1);
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - prints the power of a number recursively
+ * @x: first parameter
+ * @y: second parameter
+ * Return: Returns result or -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (1);
+}
