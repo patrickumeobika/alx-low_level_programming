d83884f77add5ae3b90155adf8bee62b5f916fc2
patrickumeobika <umeobikachidieberembj16@gmail.com>
2022-10-05 23:15:36 +0000

    tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int __attribute__((unused))  argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
